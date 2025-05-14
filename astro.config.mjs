d62b298592048d5af006d45ce6decd8e17aaedeb
ddnowicki <daniel.dominik.nowicki@gmail.com>
2025-05-14 21:48:30 +0200

    fix: change output configuration from "server" to "static" in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   integrations: [react(), sitemap()],
   server: { port: 3000 },
   vite: {
